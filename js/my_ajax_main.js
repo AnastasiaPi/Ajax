250aaf7d96a5703b6c42b40711c94afbe42da296
 Anastasia Pidgurska <anastasiapidgurska@MacBook-Pro-Anastasia.local>
2020-05-21 14:58:16 +0300

    First

---
+++ js/my_ajax_main.js
@@ -0,0 +1,22 @@
+$("#btn").click(function(event){
+  let name = $("#name").val();
+  let gender = $("#gender option:selected").text();
+  let date = $("#date").val();
+  let class1 = $("#class option:selected").text();
+  console.log(gender+" "+name+" "+date+" "+class1);
+
+
+$.ajax({
+    url:   "index.html",
+    type:     "POST",
+    dataType: "json",
+    data: {name:name,gender:gender,date:date},
+    success: function(data) { //Данные отправлены успешно
+        // console.log(data.name)
+        $('#result_form').text(data);
+    },
+    error: function(data) { // Данные не отправлены
+        $('#result_form').text('Error data is not sent');
+    }
+  });
+ });
