7b5c3593d036b6b9a50443e12fc94619331464bc
 Anastasia Pidgurska <anastasiapidgurska@MacBook-Pro-Anastasia.local>
2020-05-21 19:06:56 +0300

    Three

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var body_parser = require('body-parser');
 const app = express();
-app.listen(3600);
+app.listen(3500);
 app.use(express.static(__dirname));
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({extended: true}))
@@ -12,8 +12,13 @@ app.post("/user",function (request, response) {
 
     let user = request.body;
 
-    if (request.headers["user-agent"] == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36") {
+    if (request.headers["user-agent"] == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/38.0.2220.41") {
+      if(user.name == "Male" || user.name == "Famale"){
         response.json([user.name,user.gender,user.date]);
+      }
+      else {
+        response.json("You are not person")
+      }
 
     }else {
         response.json("Only Chrome!!!")
@@ -21,7 +26,7 @@ app.post("/user",function (request, response) {
 });
 
 app.get("/user", function(request, response){
-    if (request.headers["user-agent"] == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36") {
+    if (request.headers["user-agent"] == "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36 OPR/38.0.2220.41") {
     response.sendFile(__dirname +"/index.html");
     }else {
     response.json("Only Chrome!!!")
